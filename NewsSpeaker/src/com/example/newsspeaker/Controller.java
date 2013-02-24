63e8691be13358dc192cc859c2694927915924fb
kimuchi1203 <kimura1203@gmail.com>
2013-02-24 15:57:23 +0900

    enable to stop from click event

--- NewsSpeaker/src/com/example/newsspeaker/Controller.java
+++ NewsSpeaker/src/com/example/newsspeaker/Controller.java
@@ -3,9 +3,12 @@ package com.example.newsspeaker;
 import android.speech.tts.TextToSpeech.OnUtteranceCompletedListener;
 
 public class Controller implements OnUtteranceCompletedListener {
+	private static final int MODE_MENU = 0;
+	private static final int MODE_CHANNEL = 1;
 	private int currentChannelId = 0;
 	private boolean isPlay = false;
 	private MainActivity mainActivity;
+	private int mode = MODE_MENU;
 
 	// <- ->  : move channel
 	// button : play/stop
@@ -17,11 +20,20 @@ public class Controller implements OnUtteranceCompletedListener {
 	public void channelMove(int i) {
 		currentChannelId = (currentChannelId+i) % mainActivity.channelList.getCount();
 		mainActivity.selectChannel(currentChannelId);
+		mode = MODE_CHANNEL;
 	}
 	
 	public void setChannel(int pos) { // for click event
-		currentChannelId = pos % mainActivity.channelList.getCount();
-		mainActivity.selectChannel(currentChannelId);
+		if(MODE_MENU==mode) {
+			currentChannelId = pos % mainActivity.channelList.getCount();
+			mainActivity.selectChannel(currentChannelId);
+			mode = MODE_CHANNEL;
+		}else{
+			mainActivity.pauseTts();
+			mainActivity.showList();
+			isPlay = false;
+			mode = MODE_MENU;
+		}
 	}
 
 	public void changePlayPause() {
@@ -29,9 +41,11 @@ public class Controller implements OnUtteranceCompletedListener {
 			mainActivity.pauseTts();
 			mainActivity.showList();
 			isPlay = false;
+			mode = MODE_MENU;
 		} else {
 			mainActivity.selectChannel(currentChannelId);
 			isPlay = true;
+			mode = MODE_CHANNEL;
 		}
 	}
 
